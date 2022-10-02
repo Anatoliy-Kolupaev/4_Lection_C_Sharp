3b084fbcfb7614d6684e74d04fd067810a4f05ef
Anatoliy K <778966@mail.ru>
2022-10-02 17:02:14 +0500

    add rec

---
+++ Example_recursion/Program.cs
@@ -0,0 +1,17 @@
+﻿// факториал это 5!=5*4*3*2*1
+//                  5*4!
+//                    4*3!
+
+// Задача: Функция принимает какое то число и показывает его факториал
+
+double Factorial (int n)
+{
+    // 1!=1
+    // 0!=1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
